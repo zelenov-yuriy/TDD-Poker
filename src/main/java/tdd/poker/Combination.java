bdb33c6ed99f5c17c81b3900cb78e4a8fea11f74
zelenov-yuriy <zelenov-yuriy@mail.ru>
2024-09-28 21:48:31 +0300

    combination identification test

--- src/main/java/tdd/poker/Combination.java
+++ src/main/java/tdd/poker/Combination.java
@@ -22,8 +22,8 @@ public class Combination {
     public Combination(Card[] cards) {
         this.cards = cards;
         sortCards();
-        priority = 0;
-        strType = "strType";
+        priority = combIdent();
+        strType = typeIdent();
     }
     public int getPriority() {
         return priority;
@@ -53,5 +53,117 @@ public class Combination {
                 }
             }
         }
+    }
+
+    private int combIdent() {
+        if (isAPair()) {
+            if (isATwoPairs()) {
+                if (isAFullHouse())
+                    return 6;
+                return 2;
+            } else if (isAThree()) {
+                if (isAFour())
+                    return 7;
+                return 3;
+            }
+            return 1;
+        } else if (isAStraight()) {
+            if (isAFlush())
+                return 8;
+            return 4;
+        } else if (isAFlush())
+            return 5;
+        return 0;
+    }
+
+    private boolean isAPair() {
+        boolean result = false;
+        for (int i = 0; i < 4; i++)
+            if (cards[i].getDeckNumber() / 4 == cards[i + 1].getDeckNumber() / 4)
+                result = true;
+        return result;
+    }
+
+    private boolean isATwoPairs() {
+        boolean result = false;
+        for (int i = 0; i < 2; i++) {
+            if (cards[i].getDeckNumber() / 4 == cards[i + 1].getDeckNumber() / 4) {
+                for (int j = i + 2; j < 4; j++) {
+                    if (cards[j].getDeckNumber() / 4 == cards[j + 1].getDeckNumber() / 4)
+                        if (cards[i].getDeckNumber() / 4 != cards[j].getDeckNumber() / 4)
+                            result = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    private boolean isAThree() {
+        boolean result = false;
+        for (int i = 0; i < 3; i++)
+            if (cards[i].getDeckNumber() / 4 == cards[i + 1].getDeckNumber() / 4 &&
+                    cards[i + 1].getDeckNumber() / 4 == cards[i + 2].getDeckNumber() / 4)
+                result = true;
+        return result;
+    }
+
+    private boolean isAStraight() {
+        boolean result = true;
+        if (cards[0].getDeckNumber() / 4 < 9) {
+            for (int i = 0; i < 4; i++)
+                if (cards[i + 1].getDeckNumber() / 4 != cards[i].getDeckNumber() / 4 + 1)
+                    result = false;
+            if (cards[0].getDeckNumber() / 4 == 0 && cards[1].getDeckNumber() / 4 == 1 &&
+                    cards[2].getDeckNumber() / 4 == 2 && cards[3].getDeckNumber() / 4 == 3 &&
+                    cards[4].getDeckNumber() / 4 == 12)
+                result = true;
+        }
+        return result;
+    }
+
+    private boolean isAFlush() {
+        boolean result = true;
+        for (int i = 0; i < 4; i++)
+            if (cards[i].getDeckNumber() % 4 != cards[i + 1].getDeckNumber() % 4)
+                result = false;
+        return result;
+    }
+
+    private boolean isAFullHouse() {
+        boolean result = false;
+        if ((cards[0].getDeckNumber() / 4 == cards[1].getDeckNumber() / 4) &&
+                (cards[1].getDeckNumber() / 4 == cards[2].getDeckNumber() / 4))
+            result = true;
+        else if ((cards[2].getDeckNumber() / 4 == cards[3].getDeckNumber() / 4) &&
+                (cards[3].getDeckNumber() / 4 == cards[4].getDeckNumber() / 4))
+            result = true;
+        return result;
+    }
+
+    private boolean isAFour() {
+        boolean result = false;
+        if ((cards[0].getDeckNumber() / 4 == cards[1].getDeckNumber() / 4) &&
+                (cards[1].getDeckNumber() / 4 == cards[2].getDeckNumber() / 4) &&
+                (cards[2].getDeckNumber() / 4 == cards[3].getDeckNumber() / 4))
+            result = true;
+        else if ((cards[1].getDeckNumber() / 4 == cards[2].getDeckNumber() / 4) &&
+                (cards[2].getDeckNumber() / 4 == cards[3].getDeckNumber() / 4) &&
+                (cards[3].getDeckNumber() / 4 == cards[4].getDeckNumber() / 4))
+            result = true;
+        return result;
+    }
+
+    private String typeIdent() {
+        return switch (priority) {
+            case 1 -> "Pair";
+            case 2 -> "Two Pairs";
+            case 3 -> "Three";
+            case 4 -> "Straight";
+            case 5 -> "Flush";
+            case 6 -> "Full House";
+            case 7 -> "Four";
+            case 8 -> "Straight Flush";
+            default -> "Highest Card";
+        };
     }
 }
--- src/test/java/tdd/poker/AppTest.java
+++ src/test/java/tdd/poker/AppTest.java
@@ -43,11 +43,8 @@ class AppTest {
     public void sortCardsTest() {
         Deck deck = new Deck();
         deck.shuffle();
-        Card[] cards = {deck.getCard(0),
-                        deck.getCard(1),
-                        deck.getCard(2),
-                        deck.getCard(3),
-                        deck.getCard(4)};
+        Card[] cards =
+                {deck.getCard(0), deck.getCard(1), deck.getCard(2), deck.getCard(3), deck.getCard(4)};
         Combination combination = new Combination(cards);
         boolean exceptionThrown = false;
         try {
@@ -58,5 +55,52 @@ class AppTest {
         assertFalse(exceptionThrown);
         System.out.print(combination);
         System.out.println("sortCardsTest - passed");
+    }
+
+    @Test
+    public void combIdentTest() {
+        Deck deck = new Deck();
+        Card[] highestCard =
+                {deck.getCard(26), deck.getCard(17), deck.getCard(0), deck.getCard(50), deck.getCard(34)};
+        Card[] pair =
+                {deck.getCard(34), deck.getCard(50), deck.getCard(26), deck.getCard(0), deck.getCard(1)};
+        Card[] twoPairs =
+                {deck.getCard(0), deck.getCard(5), deck.getCard(1), deck.getCard(39), deck.getCard(7)};
+        Card[] three =
+                {deck.getCard(1), deck.getCard(0), deck.getCard(39), deck.getCard(7), deck.getCard(2)};
+        Card[] straight1 =
+                {deck.getCard(31), deck.getCard(33), deck.getCard(21), deck.getCard(24), deck.getCard(38)};
+        Card[] straight2 =
+                {deck.getCard(5), deck.getCard(1), deck.getCard(49), deck.getCard(9), deck.getCard(14)};
+        Card[] flush =
+                {deck.getCard(17), deck.getCard(9), deck.getCard(37), deck.getCard(29), deck.getCard(1)};
+        Card[] fullHouse =
+                {deck.getCard(44), deck.getCard(45), deck.getCard(0), deck.getCard(47), deck.getCard(3)};
+        Card[] four =
+                {deck.getCard(16), deck.getCard(19), deck.getCard(42), deck.getCard(18), deck.getCard(17)};
+        Card[] straightFlush =
+                {deck.getCard(33), deck.getCard(41), deck.getCard(37), deck.getCard(45), deck.getCard(29)};
+
+        Combination highestCardComb = new Combination(highestCard);
+        Combination pairComb = new Combination(pair);
+        Combination twoPairsComb = new Combination(twoPairs);
+        Combination threeComb = new Combination(three);
+        Combination straight1Comb = new Combination(straight1);
+        Combination straight2Comb = new Combination(straight2);
+        Combination flushComb = new Combination(flush);
+        Combination fullHouseComb = new Combination(fullHouse);
+        Combination fourComb = new Combination(four);
+        Combination straightFlushComb = new Combination(straightFlush);
+
+        System.out.println(highestCardComb);
+        System.out.println(pairComb);
+        System.out.println(twoPairsComb);
+        System.out.println(threeComb);
+        System.out.println(straight1Comb);
+        System.out.println(straight2Comb);
+        System.out.println(flushComb);
+        System.out.println(fullHouseComb);
+        System.out.println(fourComb);
+        System.out.println(straightFlushComb);
     }
 }
