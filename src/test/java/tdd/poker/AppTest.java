475eba64e20c956639aaf35b732925993b10fbb8
zelenov-yuriy <zelenov-yuriy@mail.ru>
2024-09-28 19:37:13 +0300

    deck shuffle test

--- src/test/java/tdd/poker/AppTest.java
+++ src/test/java/tdd/poker/AppTest.java
@@ -15,13 +15,27 @@ class AppTest {
 
     @AfterEach
     void tearDown() {
-        System.out.println("tearDown");
+        System.out.println("tearDown \n");
     }
 
     @Test
-    public void testCardCreation() {
+    public void cardCreationTest() {
         Card card = new Card(0, "2", "clubs");
         String result = card.toString();
         assertEquals("0) 2 of clubs", result);
+        System.out.println("cardCreationTest - passed");
+    }
+
+    @Test
+    public void deckShuffleTest() {
+        Deck deck = new Deck();
+        boolean exceptionThrown = false;
+        try {
+                deck.shuffle();
+        } catch (IndexOutOfBoundsException e) {
+            exceptionThrown = true;
+        }
+        assertFalse(exceptionThrown);
+        System.out.println("deckShuffleTest - passed");
     }
 }
